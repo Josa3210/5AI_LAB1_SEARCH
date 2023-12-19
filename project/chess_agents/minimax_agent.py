58b2febc79cd93c4d5c7daf2dc23fc856963f570
Tobias2512 <hagenaarstobias@gmail.com>
2023-12-19 13:57:16 +0100

    added minimax function

--- project/chess_agents/minimax_agent.py
+++ project/chess_agents/minimax_agent.py
@@ -1,10 +1,17 @@
 
 import chess
 from project.chess_agents.agent import Agent
+import time
+from project.chess_utilities.utility import Utility
 
 
 class MiniMaxAgent(Agent):
-    
+
+    def __init__(self, utility: Utility, time_limit_move: float) -> None:
+        super().__init__(utility, time_limit_move)
+        self.name = "ML Bot"
+        self.author = "Neural Network Ninjas"
+
     def calculate_move(self, board: chess.Board) -> chess.Move:
         # TODO Minimax:
         # - possible extras:
@@ -12,4 +19,65 @@ class MiniMaxAgent(Agent):
         #   - Move ordering
         #   - iterative deepening
         # use self.utility.board_value(board) to get an evaluation of the board.
-        return list(board.legal_moves)[0]
+        Start_time = time.time()
+        depth = 3
+        best_move = None
+        best_utility = float("-inf") if board.turn == chess.WHITE else float("inf")
+        for Current_depth in range(1, depth + 1):  # Iterative Deepening
+            # If the agent is playing as black, the utility values are flipped (negative-positive)
+            flip_value = 1 if board.turn == chess.WHITE else -1
+            move, utility = self.MiniMax(board, Current_depth, float("-inf"), float("inf"), flip_value, Start_time)
+            if move is not None:
+                if (flip_value == 1 and utility > best_utility) or (flip_value == -1 and utility < best_utility):
+                    best_move = move  # best move update als utility beter is dan vorige best_utility
+                    best_utility = utility
+        return best_move
+
+    def MiniMax(self, board, depth, alpha, beta, flip_value, Start_time) -> tuple[chess.Move, float]:
+        if depth == 0 or board.is_terminal():
+            return None, flip_value * self.utility.board_value(board)
+
+        if flip_value == 1:  # When white -> Maximizing
+            return self.Max_value(board, depth, alpha, beta, flip_value, Start_time)
+        else:  # When Black -> Minimizing
+            return self.Min_Value(board, depth, alpha, beta, flip_value, Start_time)
+
+    def Max_value(self, board, depth, alpha, beta, flip_value, Start_time) -> tuple[chess.Move, float]:
+        best_move = None
+        best_utility = float("-inf")
+        # Loop through all legal moves
+        for move in list(board.legal_moves):
+            # Check if the maximum calculation time for this move has been reached
+            if time.time() - Start_time > self.time_limit_move:
+                break
+            # Play the move
+            board.push(move)
+            _, value = self.MiniMax(board, depth - 1, alpha, beta, -flip_value, Start_time)
+            board.pop()
+            if value > best_utility:
+                best_move, best_utility = move, value
+            alpha = max(alpha, best_utility)
+            if beta <= alpha:
+                break
+
+        return best_move, best_utility
+
+    def Min_Value(self, board, depth, alpha, beta, flip_value, Start_time):
+        best_move = None
+        best_utility = float("inf")
+        # Loop through all legal moves
+        for move in list(board.legal_moves):
+            # Check if the maximum calculation time fot this move has been reached
+            if time.time() - Start_time > self.time_limit_move:
+                break
+            # Play the move
+            board.push(move)
+            _, value = self.MiniMax(board, depth - 1, alpha, beta, -flip_value, Start_time)
+            board.pop()
+            if value < best_utility:
+                best_move, best_utility = move, value
+            beta = min(beta, best_utility)
+            if beta <= alpha:
+                break
+
+        return best_move, best_utility
