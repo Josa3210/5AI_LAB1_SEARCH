c447765bae8f5b79a6a31a463bdb3244e8a27028
vincent verbergt <vincent.verbergt@gmail.com>
2024-01-20 12:08:37 +0100

    added pause to the program when pressing space.
    the program will spinlock with 2 sec sleep intervals.

--- project/scripts/train_data_set.py
+++ project/scripts/train_data_set.py
@@ -3,6 +3,9 @@ import os
 import time
 from math import floor
 from typing import Type
+import keyboard
+from time import sleep
+from os import _exit
 
 import numpy as np
 from matplotlib import pyplot as pt
@@ -20,6 +23,11 @@ import matplotlib.pyplot as plt
 from project.machine_learning.neural_network_heuristic import NeuralNetworkHeuristic, CanCaptureHeuristic, CanCaptureHeuristicBit, WorldViewHeuristic
 from project.machine_learning.parsing import ChessDataLoader, DataParser
 
+isPaused = False
+def pausePoint():
+    global isPaused
+    while isPaused:
+        sleep(2)
 
 class Criterion:
     def __call__(self, results: t.Tensor, targets: t.Tensor) -> t.Tensor:
@@ -51,6 +59,7 @@ def train(model: nn.Module, optimizer: Optimizer, criterion: Criterion, dataLoad
     batch = 0
     model.train()
     for j, data in enumerate(dataLoader):
+        pausePoint()
         inputs, targets = data
         optimizer.zero_grad()
 
@@ -76,7 +85,8 @@ def train(model: nn.Module, optimizer: Optimizer, criterion: Criterion, dataLoad
             print(" " * 130, end="\r")
             batchLoss = _reportLoss / reportingPeriod
             _reportLoss = 0
-            print(f"{batch}: Average loss over last {reportingPeriod} batches: {round(batchLoss, 5)} ")
+            print(
+                f"{batch}: Average loss over last {reportingPeriod} batches: {round(batchLoss, 5)} ")
     print(" " * 130, end="\r")
     _averageTrainingLoss = _runningLoss / (len(dataLoader)//batchSize)
     return round(_averageTrainingLoss, 5)
@@ -87,6 +97,7 @@ def validate(model: nn.Module, criterion: Criterion, validationDataLoader: DataL
     model.eval()
     with torch.no_grad():
         for j, data in enumerate(validationDataLoader):
+            pausePoint()
             inputs, targets = data
 
             with autocast():
@@ -95,7 +106,8 @@ def validate(model: nn.Module, criterion: Criterion, validationDataLoader: DataL
 
             _validationLoss += loss.item()
             if j % 20:
-                percentage = floor(j / (len(validationDataLoader)//batchSize) * 100)
+                percentage = floor(
+                    j / (len(validationDataLoader)//batchSize) * 100)
                 print("Evaluating: {", "=" * percentage,
                       " " * (100 - percentage), "}", end='\r')
 
@@ -106,6 +118,7 @@ def collectData(folder_path: str, heuristic: Type[NeuralNetworkHeuristic], batch
     files = os.listdir(folder_path)
     dataParsers: [DataParser] = []
     for file in files:
+        pausePoint()
         if not file.split(".")[-1] == "cache":
             dataParser = DataParser(filePath=folder_path + "/" + file)
             dataParser.parse()
@@ -114,18 +127,21 @@ def collectData(folder_path: str, heuristic: Type[NeuralNetworkHeuristic], batch
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog="DataTrainer", description="Train our model using datasets located in /project/data/raw")
+    parser = argparse.ArgumentParser(
+        prog="DataTrainer", description="Train our model using datasets located in /project/data/raw")
     parser.add_argument('-l', '--learning-rate', default=0.0001, type=float)
     parser.add_argument('-b', '--batch-size', default=32, type=int)
     parser.add_argument('-e', '--epochs', default=5, type=int)
-    parser.add_argument('--preload', default=None, type=str, help="Continue training on a previous model, value is location to model")
+    parser.add_argument('--preload', default=None, type=str,
+                        help="Continue training on a previous model, value is location to model")
     args = parser.parse_args()
     learningRate = args.learning_rate
     batchSize = args.batch_size
     numberOfEpochs = args.epochs
     preload = (args.preload)
 
-    print(f"The learning parameters are:\n- Learning rate:\t{learningRate}\n- batchSize:\t\t{batchSize}\n- number of epochs:\t{numberOfEpochs}\n- preload:\t\t{preload}\n")
+    print(
+        f"The learning parameters are:\n- Learning rate:\t{learningRate}\n- batchSize:\t\t{batchSize}\n- number of epochs:\t{numberOfEpochs}\n- preload:\t\t{preload}\n")
 
     # TODO use the dataset to train a NeuralNetworkHeuristic, afterwards save it.
 
@@ -145,13 +161,41 @@ if __name__ == '__main__':
     optimizer = Adam(model.parameters(), lr=learningRate)
     criterion: Criterion = nn.MSELoss()
 
+    # Implement pause at any moment loop
+    
+    def on_press_space(e):
+        global isPaused, model
+        if isPaused:
+            return
+        isPaused = True
+        res = input(
+            "You paused the program, do you wish to stop and save? [y/n]: ")
+        if "y" in res:
+            res = input("Under what name do you want to save the model?: ")
+            saveLocation = f"project/data/models/{res}"
+            torch.save(model, saveLocation)
+            print(f"Saved the current model under {saveLocation}")
+            _exit(0)
+        else:
+            res = input("Do you wish to continue? [y/n]: ")
+            while res != "y":
+                res = input("Do you wish to continue? [y/n]: ")
+            isPaused = False
+    keyboard.on_press_key(key='space', callback=on_press_space)
+    
+    print("If you want to pause training at any moment press SPACE")
+
     # Specify the folder path you want to get filepaths from
     trainingFolderPath = "project/data/raw/training"
     validationFolderPath = "project/data/raw/validation"
 
-    trainDataLoader = collectData(trainingFolderPath, model.__class__, batchSize)
-    validationDataLoader = collectData(validationFolderPath, model.__class__, batchSize)
-    print(f"Total amount of batches:\n- training:\t{len(trainDataLoader)} datapoints -> {len(trainDataLoader)//batchSize} batches\n- validation:\t{len(validationDataLoader)} datapoints -> {len(validationDataLoader)//batchSize} batches\n")
+    trainDataLoader = collectData(
+        trainingFolderPath, model.__class__, batchSize)
+    validationDataLoader = collectData(
+        validationFolderPath, model.__class__, batchSize)
+    print(
+        f"Total amount of batches:\n- training:\t{len(trainDataLoader)} datapoints -> {len(trainDataLoader)//batchSize} batches\n- validation:\t{len(validationDataLoader)} datapoints -> {len(validationDataLoader)//batchSize} batches\n")
+
 
     print("Start training: \n" + "=" * 100, '\n')
     startTime = time.perf_counter()
@@ -160,25 +204,32 @@ if __name__ == '__main__':
     validationLossValues = []
 
     for i in range(numberOfEpochs):
+        pausePoint()
         print(f"Starting epoch {i + 1}:\n" + "-" * 100)
         averageTrainingLoss = train(model=model, optimizer=optimizer,
                                     criterion=criterion, dataLoader=trainDataLoader)
-        print(f"Finished training for epoch {i + 1} with average training loss: {averageTrainingLoss}")
+        print(
+            f"Finished training for epoch {i + 1} with average training loss: {averageTrainingLoss}")
         trainingLossValues.append(averageTrainingLoss)
 
-        averageValidationLoss = validate(model=model, validationDataLoader=validationDataLoader, criterion=criterion)
+        averageValidationLoss = validate(
+            model=model, validationDataLoader=validationDataLoader, criterion=criterion)
         validationLossValues.append(averageValidationLoss)
-        print(f"Finished validating for epoch {i + 1} with average validation loss: {averageValidationLoss}" + " " * 50 + "\n")
-        torch.save(model, f"project/data/models/{model.getName()}_{i + 1}_0,{round(averageValidationLoss * 10000)}")
+        print(
+            f"Finished validating for epoch {i + 1} with average validation loss: {averageValidationLoss}" + " " * 50 + "\n")
+        torch.save(
+            model, f"project/data/models/{model.getName()}_{i + 1}_0,{round(averageValidationLoss * 10000)}")
         if earlyStopper is not None and earlyStopper.early_stop(averageValidationLoss):
-            print(f"Stopped early because previous loss {earlyStopper.minLoss} was lower than current loss {averageValidationLoss}")
+            print(
+                f"Stopped early because previous loss {earlyStopper.minLoss} was lower than current loss {averageValidationLoss}")
             break
 
     endTime = time.perf_counter()
     seconds = endTime - startTime
     minutes = seconds // 60
     seconds = seconds - minutes * 60
-    print(f"Time passed training: {round(minutes)} minutes {round(seconds)} seconds")
+    print(
+        f"Time passed training: {round(minutes)} minutes {round(seconds)} seconds")
 
     # Plot the loss for every epoch
     pt.plot(trainingLossValues, 'r', label="training loss")
